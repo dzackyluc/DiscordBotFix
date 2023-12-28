580882e7ab8f0ae52f330c02913efdf9ab01b06c
dzackyluc <dzackylucifer12345@gmail.com>
2023-12-29 00:59:14 +0700

    bug fix

--- cogs/quran.py
+++ cogs/quran.py
@@ -28,34 +28,31 @@ class quran(commands.Cog):
     @commands.hybrid_command(name="quran", description="Dapatkan ayat al-quran berdasarkan surat")
     async def quran(self, interaction: discord.Interaction, nomor: int):
         await interaction.interaction.response.defer()
-        try:
-            if nomor >= 1 or nomor <= 114:
-                hasil = alquran(nomor)
-                namalatin = hasil['data']['namaLatin']
-                deskripsi = hasil['data']['deskripsi']
-                replacements = str(deskripsi).replace("<i>", "_").replace("</i>", "_")
-                ayats = hasil['data']['ayat']
-                audio = hasil['data']['audioFull']['01']
-                mbed = discord.Embed(title=f"{namalatin}",
-                      description=f"{replacements}",
-                      colour=0x00b0f4,
-                      timestamp=datetime.datetime.now())
-                mbed.set_author(name=interaction.message.author, icon_url=interaction.message.author.avatar)
-                for ayat in ayats:
-                    teks_arab = ayat['teksArab']
-                    teks_latin = ayat['teksLatin']
-                    mbed.add_field(name=f"{teks_arab}",
-                                   value=f"{teks_latin}",
-                                   inline=False)
-                mbed.add_field(name="Link Audio Baca",
-                               value=f"> {audio}",
+        if nomor >= 1 or nomor <= 114:
+            hasil = alquran(nomor)
+            namalatin = hasil['data']['namaLatin']
+            deskripsi = hasil['data']['deskripsi']
+            replacements = str(deskripsi).replace("<i>", "_").replace("</i>", "_")
+            ayats = hasil['data']['ayat']
+            audio = hasil['data']['audioFull']['01']
+            mbed = discord.Embed(title=f"{namalatin}",
+                  description=f"{replacements}",
+                  colour=0x00b0f4,
+                  timestamp=datetime.datetime.now())
+            mbed.set_author(name=interaction.message.author, icon_url=interaction.message.author.avatar)
+            for ayat in ayats:
+                teks_arab = ayat['teksArab']
+                teks_latin = ayat['teksLatin']
+                mbed.add_field(name=f"{teks_arab}",
+                               value=f"{teks_latin}",
                                inline=False)
-                mbed.set_footer(text=self.client.user.name, icon_url=self.client.user.avatar)
-                await interaction.interaction.followup.send(embed=mbed)
-            else:
-                await interaction.interaction.followup.send("Pilih dari surat 1 sampai surat ke 114")
-        except:
-            await interaction.interaction.followup.send("Error commands could not be loaded")
+            mbed.add_field(name="Link Audio Baca",
+                           value=f"> {audio}",
+                           inline=False)
+            mbed.set_footer(text=self.client.user.name, icon_url=self.client.user.avatar)
+            await interaction.interaction.followup.send(embed=mbed)
+        else:
+            await interaction.interaction.followup.send("Pilih dari surat 1 sampai surat ke 114")
 
 
 async def setup(client):
